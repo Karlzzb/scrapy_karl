3d95630522d4ad064dba7b7bca13f677fbfd7dbe
Karlzzb <lansedepiaomiao@gmail.com>
2017-12-25 20:40:40 +0800

    fetch jd use agent

--- tutorial/spiders/jd_spider.py
+++ tutorial/spiders/jd_spider.py
@@ -9,7 +9,7 @@ import requests
 #---------------------------------------------------------------------------
 class JdSpider(scrapy.Spider):
     name = "test"
-    allowed_domains = ["jd.com"]
+    allowed_domains = ["jd.com","p.3.cn"]
     start_urls = [
         "https://list.jd.com/list.html?cat=9987,653,655"
     ]
@@ -17,13 +17,12 @@ class JdSpider(scrapy.Spider):
     def parse(self,response):
         '分别获得商品的地址和下一页地址'
         req = []
-        for i in range(0, 10):
+        for i in range(1, 2):
             url = "https://list.jd.com/list.html?cat=9987,653,655&page=" + str(i+1) + "&sort=sort%5Frank%5Fasc&trans=1&JL=6_0_0&ms=6"
             r = Request(url, callback=self.parse)
             req.append(r)
 
         '商品地址'
-        logging.log(logging.DEBUG, response.xpath('//div[contains(@class, "p-img")]/a/@href').extract())
         for sel in response.xpath('//div[contains(@class, "p-img")]/a'):
             for i in sel.xpath('@href').extract():
                 url = "https://" + i
@@ -34,21 +33,29 @@ class JdSpider(scrapy.Spider):
 
     def parse_product(self,response):
         '商品页获取title,price,product_id'
+        req = []
         phonename = response.xpath('//div/ul[contains(@class, "parameter2 p-parameter-list")]/li[1]/@title').extract_first()
         issueyear = response.xpath('//div[contains(@class, "Ptable-item")][1]/dl/dd/text()').re(u'[1-9][0-9][0-9][1-9]\u5e74')
         issuemonth = response.xpath('//div[contains(@class, "Ptable-item")][1]/dl/dd/text()').re(u'[1-9][0-9]*\u6708')
         pattern = r"(\d+)\.html$"
         id = re.findall(pattern, response.url) 
         priceUrl = "https://p.3.cn/prices/mgets?&skuIds=J_"+str(id[0])
-        priceData = requests.get(priceUrl).content.decode("utf-8", "ignore")
-        patt = r'"p":"(\d+\.\d+)"'
-        price = re.findall(patt, priceData)
-        logging.log(logging.DEBUG, id)
         item = TutorialItem()
         item['phonename'] = phonename
         item['issueyear'] = issueyear
         item['issuemonth'] = issuemonth
-        item['price'] = price
         item['itemurl'] = response.url
+        request = scrapy.Request(priceUrl, method="GET", callback=self.parse_price)
+        request.meta['item'] = item
+        logging.log(logging.DEBUG, request)
+        yield request
+
+    def parse_price(self,response):
+        logging.log(logging.DEBUG, response.body)
+        item = response.meta['item']
+        priceData = response.body
+        patt = r'"p":"(\d+\.\d+)"'
+        price = re.findall(patt, str(priceData))
+        item['price'] = price
         return item
 ############################################################################
